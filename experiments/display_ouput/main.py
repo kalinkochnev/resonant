ccb4c57a412ab68fc24a222bf9490a2d4d6a83ba
rmenon1008 <rmenon1008@gmail.com>
2021-03-05 00:40:38 +0000

    Updated for multicolor display

--- experiments/display_ouput/main.py
+++ experiments/display_ouput/main.py
@@ -11,7 +11,7 @@ import textwrap
 
 class Display():
     def __init__(self):
-        self.disp = Adafruit_SSD1306.SSD1306_128_32(rst=None)
+        self.disp = Adafruit_SSD1306.SSD1306_128_64(rst=None)
         self.disp.begin()
         self.disp.clear()
         self.disp.display()
@@ -31,41 +31,38 @@ class Display():
         draw = ImageDraw.Draw(image)
         padding = 5
         ellipsis_width = width-padding
-        ellipsis_height = height-padding
+        ellipsis_height = height-padding - 20
 
         # Clear the screen
         draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-        # Draw the base ellipse
+        # Draw the base ellipse and direction arrow
         draw.ellipse((padding, padding, ellipsis_width, ellipsis_height), outline=255, fill=0)
+        draw.polygon((width/2 - 8,24,width/2 + 8,24,width/2,17), outline=255, fill=0)
 
         # Draw the dot indicating position
         a = self.angle*(math.pi/180)
-        x = math.cos(a) * (ellipsis_width/2 - 4) + width/2
-        y = -math.sin(a) * (ellipsis_height/2 - 4) + height/2
-        draw.ellipse((x-4, y-4, x+4, y+4), outline=255, fill=1)
+        x = math.cos(a) * ((ellipsis_width-padding)/2) + width/2
+        y = -math.sin(a) * ((ellipsis_height-padding)/2) + (height-20)/2
+        draw.ellipse((x-5, y-5, x+5, y+5), outline=255, fill=1)
 
         # Display the text
         char_width = 8
         char_height = 16
         # font = ImageFont.load_default()
-        font = ImageFont.truetype('freepixel.ttf', char_height)
-        lines = textwrap.wrap(self.sound, width=width/char_width)[:2]
+        font = ImageFont.truetype('freepixel-modified.ttf', char_height)
 
-        start_y = (height - len(lines)*char_height)/2
-
-        for l in range(0,len(lines)):
-            x = (width - (len(lines[l]) * char_width)) / 2
-            draw.text((x, start_y + l * (char_height-2)), lines[l],  font=font, fill=255)
+        x = (width - (len(self.sound) * char_width)) / 2
+        draw.text((x, height-18), self.sound,  font=font, fill=255)
 
         # Push the image to the display
-        self.disp.image(image)
+        self.disp.image(image.rotate(180))
         self.disp.display()
     
     # def updateHeading(self):
-    #     heading = #TODO GET IMU HEADING
-    #     angle_change = self.original_heading - heading
-    #     self.put(self.angle - angle_change, self.sound)
+    #     current_heading = #TODO GET IMU HEADING
+    #     angle_adjust = self.original_heading - current_heading
+    #     self.put(self.angle - angle_adjust, self.sound)
 
 display = Display()
-display.put(270,"Car Honking")
+display.put(90,"\"Car Honking\"")
