4347e7a429eec2f184f9f442257acafe9cf24040
kalinkochhnev <kalin.kochnev@gmail.com>
2021-03-07 21:52:41 -0500

    Figured out how to steam live audio data

--- resonant/main.py
+++ resonant/main.py
@@ -1,29 +1,73 @@
 from source.initialization import OfflineAudioIter
 from source.algorithms import SphereTester
+import source.constants as const
+import pyaudio
+import numpy as np
+import time
+
+lw_size = const.SAMPLING_RATE * const.LARGE_WINDOW
+large_window = np.zeros(lw_size)
+
+
+def reader(in_data, frame_count, time_info, status):
+    global large_window
+    new_data = np.frombuffer(in_data, dtype=np.int16)
+    updated = np.concatenate((new_data, large_window[:lw_size-frame_count]))
+    large_window = updated
+    print(f"{large_window} ---- size: {large_window[large_window !=0].size} ----- arr siez {large_window.size}")
+
+    return (in_data, pyaudio.paContinue)
+
+
+def choose_audio_device(p_audio):
+    info = p_audio.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range(0, numdevices):
+        if (p_audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, " - ",
+                  p_audio.get_device_info_by_host_api_device_index(0, i).get('name'))
+    return int(input("What input id do you choose?"))
+
 
 if __name__ == "__main__":
     # Load audio file
     audio_iter = OfflineAudioIter('../data/dog_barking_90/combined.wav')
     mics = audio_iter.initialize_mics()
-    localization = SphereTester(mics)
-
-    import matplotlib.pyplot as plt
-    plt.scatter([], [])
-    plt.show()
+    """localization = SphereTester(mics)
 
     for window in audio_iter:
         localization.update_signals(window)
-        localization.run()
+        localization.run()"""
 
-    
+    # Init resources
+    # ml_resources = ml.init()
+    py_aud = pyaudio.PyAudio()
+    audio_stream = py_aud.open(format=const.AUDIO_FORMAT, channels=const.NUM_MICS,
+                               rate=const.SAMPLING_RATE, input=True, frames_per_buffer=const.AUDIO_FRAME_SIZE, 
+                               stream_callback=reader, input_device_index=choose_audio_device(py_aud))
+    audio_stream.start_stream()
+    while audio_stream.is_active():
+        time.sleep(0.2)
 
-    # Loop with window size
-        # Calculate cross corr of pair 1
-        # Calculate cross corr of pair 2
+    audio_stream.stop_stream()
+    audio_stream.close()
+    py_aud.terminate()
 
-        # Loop through different layers
-            # Loop through circle's points
-                # For each mic pair calculate delays of that point
-                # Convert delay to # of samples
+    # Main thread that reads audio
 
+    # LOCATOR THREAD: reads data into buffer
 
+    # if sound is localized
+    # Add untracked source
+    # start ML thread
+
+    # ML Reader: reads data into buffer
+    # Once evaluation time (buffer is filled or updated)
+    # for un/tracked sounds
+    # Beamform sound and start ML THREAD:
+    # evaluate model. If sound found, return sound
+
+    # If sound is found
+    # add tracked source
+
+    # Back to locator
--- resonant/source/constants.py
+++ resonant/source/constants.py
@@ -1,15 +1,18 @@
 import math
-
+import pyaudio
 V_SOUND = 343  # speed of sound
 NUM_SLICES = 20  # of slices to divide circle into
 MIC_SPACING = 0.062  # meters
-NUM_MICS = 4
+NUM_MICS = 1
 SAMPLING_RATE = 22050  # of samples taken per second for each channel
 WINDOW_SIZE = 800 # This is the number of samples to run the algorithm on
-
 # TODO not done
 # 0 means no interpolation, 1 means one additional index for each amplitude
 INTERPOLATION_AMOUNT = 0
+
+AUDIO_FORMAT = pyaudio.paInt16
+AUDIO_FRAME_SIZE = 5000
+LARGE_WINDOW = 5 #  # of seconds
 
 import source.geometry as geom
 MIC_POSITIONS = [
