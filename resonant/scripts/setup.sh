d5c7e392a77294e9a4f626879743950560512b70
kalinkochhnev <kalin.kochnev@gmail.com>
2021-05-25 13:30:35 -0400

    Minor adjustments to setup and installed packages

---
+++ resonant/scripts/setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Make sure to run     chmod a+x install.sh     before running
+# To execute  ./install.sh
+# Make sure this is run at the root directory
+echo "----------------------------- Checking for updates first... -----------------------------"
+sudo apt-get update
+
+REQUIRED_PACKAGES=( 
+    python3 python3-pip python3-venv git python3-dev
+    libssl-dev libatlas-base-dev # Numpy
+    libopenjp2-7 libtiff5 # Pillow
+    portaudio19-dev python3-pyaudio # libportaudio0 libportaudio2 libportaudiocpp0 libasound2-dev #Pyaudio
+)
+
+# IF YOU ARE RUNNING THIS ON A PI
+# git clone https://github.com/respeaker/seeed-voicecard
+# cd seeed-voicecard
+# sudo ./install.sh
+# sudo reboot
+
+function checkOrInstall() {
+    REQUIRED_PKG=$1
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok")
+    echo Checking for $REQUIRED_PKG: $PKG_OK
+    if [ "" = "$PKG_OK" ]; then
+    echo "$REQUIRED_PKG not found. Setting up $REQUIRED_PKG..."
+    sudo apt-get --yes install $REQUIRED_PKG 
+    fi
+}
+
+for PACKAGE in ${REQUIRED_PACKAGES[@]}
+do
+    checkOrInstall $PACKAGE
+done
+
+echo "----------------------------- Linux packages installed -----------------------------"
+if [ ! -d "env/" ]; then
+    echo "Creating virtual env..."
+    python3 -m venv env/
+fi
+
+. ./env/bin/activate
+echo "----------------------------- Installing pip packages -----------------------------"
+sudo pip3 install -r requirements.txt
+
+echo "----------------------------- All done! -----------------------------"
